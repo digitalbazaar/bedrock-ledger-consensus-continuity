ce9abd8eed9431f3a0ce1b7537ba64ea76e718b9
Matthew Collier <collier.matthew@gmail.com>
2017-07-07 17:46:13 -0400

    Fix deps in test suite.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,32 @@
 /*
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
-var bedrock = require('bedrock');
+const bedrock = require('bedrock');
+const brDidClient = require('bedrock-did-client');
 require('bedrock-ledger-continuity');
 require('bedrock-ledger-storage-mongodb');
+require('bedrock-ledger-context');
+
+bedrock.events.on('bedrock.init', () => {
+  const jsonld = bedrock.jsonld;
+  const mockData = require('./mocha/mock.data');
+
+  const oldLoader = jsonld.documentLoader;
+
+  jsonld.documentLoader = function(url, callback) {
+    if(Object.keys(mockData.ldDocuments).includes(url)) {
+      return callback(null, {
+        contextUrl: null,
+        document: mockData.ldDocuments[url],
+        documentUrl: url
+      });
+    }
+    oldLoader(url, callback);
+  };
+  // override jsonld.documentLoader in brDidClient so this document loader
+  // can be used for did: and https: URLs
+  brDidClient.jsonld.documentLoader = jsonld.documentLoader;
+});
 
 require('bedrock-test');
 bedrock.start();
