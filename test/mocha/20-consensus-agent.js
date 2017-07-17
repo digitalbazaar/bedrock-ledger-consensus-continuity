8d22768b37fc626b5ee63b64f36fd8c6f8696b48
Matthew Collier <collier.matthew@gmail.com>
2017-07-17 15:58:11 -0400

    Start consensus-agent tests.

---
+++ test/mocha/20-consensus-agent.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+let request = require('request');
+request = request.defaults({json: true, strictSSL: false});
+
+describe.only('Consensus Agent API', () => {
+  it('works');
+});
