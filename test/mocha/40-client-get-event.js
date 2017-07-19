95e08f746366b30408184b254963939841093cfa
Matthew Collier <collier.matthew@gmail.com>
2017-07-19 15:34:40 -0400

    Add client test.

---
+++ test/mocha/40-client-get-event.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+/* globals should */
+'use strict';
+
+const async = require('async');
+const bedrock = require('bedrock');
+const brLedger = require('bedrock-ledger');
+const helpers = require('./helpers');
+const mockData = require('./mock.data');
+const uuid = require('uuid/v4');
+
+describe.only('Consensus Client - getEvent API', () => {
+  before(done => {
+    helpers.prepareDatabase(mockData, done);
+  });
+
+  let consensusApi;
+  let ledgerNode;
+  let voterId;
+  let eventHash;
+  let testEventId;
+  beforeEach(done => {
+    const configEvent = mockData.events.config;
+    const testEvent = bedrock.util.clone(mockData.events.alpha);
+    testEventId = 'https://example.com/events/' + uuid();
+    testEvent.input[0].id = testEventId;
+    async.auto({
+      clean: callback =>
+        helpers.removeCollections(['ledger', 'ledgerNode'], callback),
+      consensusPlugin: callback =>
+        brLedger.use('Continuity2017', (err, result) => {
+          if(err) {
+            return callback(err);
+          }
+          consensusApi = result.api;
+          callback();
+        }),
+      ledgerNode: ['clean', (results, callback) => brLedger.add(
+        null, configEvent, (err, result) => {
+          if(err) {
+            return callback(err);
+          }
+          ledgerNode = result;
+          callback();
+        })],
+      getVoter: ['consensusPlugin', 'ledgerNode', (results, callback) => {
+        consensusApi._worker._voters.get(ledgerNode.id, (err, result) => {
+          voterId = result.id;
+          callback();
+        });
+      }],
+      addEvent: ['ledgerNode', (results, callback) => ledgerNode.events.add(
+        testEvent, (err, result) => {
+          eventHash = result.meta.eventHash;
+          callback();
+        })]
+    }, done);
+  });
+  it('should get an event', done => {
+    async.auto({
+      get: callback => consensusApi._worker._client.getEvent(
+        eventHash, {id: voterId}, (err, result) => {
+          should.not.exist(err);
+          should.exist(result);
+          result.should.be.an('object');
+          result.input.should.be.an('array');
+          result.input.should.have.length(1);
+          result.input[0].should.be.an('object');
+          result.input[0].id.should.equal(testEventId);
+          callback();
+        })
+    }, done);
+  });
+  it('returns an error on an unknown event', done => {
+    async.auto({
+      get: callback => consensusApi._worker._client.getEvent(
+        uuid(), {id: voterId}, (err, result) => {
+          should.exist(err);
+          should.not.exist(result);
+          err.details.httpStatusCode.should.equal(404);
+          callback();
+        })
+    }, done);
+  });
+});
