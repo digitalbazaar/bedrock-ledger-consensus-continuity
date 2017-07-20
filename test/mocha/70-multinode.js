0a6cea3d2119eaf64ab1d1b4ddc8ccba6ef402a4
Dave Longley <dlongley@digitalbazaar.com>
2017-07-20 14:42:21 -0400

    Add multinode test.

---
+++ test/mocha/70-multinode.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+/* globals should */
+
+'use strict';
+
+const bedrock = require('bedrock');
+const brIdentity = require('bedrock-identity');
+const brLedger = require('bedrock-ledger');
+const async = require('async');
+const expect = global.chai.expect;
+const uuid = require('uuid/v4');
+
+const helpers = require('./helpers');
+const mockData = require('./mock.data');
+
+describe.skip('Multinode', () => {
+  before(done => {
+    helpers.prepareDatabase(mockData, done);
+  });
+
+  describe('Consensus with 4 Nodes', () => {
+    const nodes = 4;
+    const maxConsensusTime = 1000;
+
+    // get consensus plugin and create genesis ledger node
+    let consensusApi;
+    let genesisLedgerNode;
+    const mockIdentity = mockData.identities.regularUser;
+    const configEvent = mockData.events.config;
+    let commonActor;
+    before(done => {
+      async.auto({
+        clean: callback =>
+          helpers.removeCollections(['ledger', 'ledgerNode'], callback),
+        actor: ['clean', (results, callback) => brIdentity.get(
+          null, mockIdentity.identity.id, (err, identity) => {
+            callback(err, identity);
+          })],
+        consensusPlugin: callback => brLedger.use('Continuity2017', callback),
+        ledgerNode: ['actor', (results, callback) => brLedger.add(
+          results.actor, {configEvent}, (err, ledgerNode) => {
+            if(err) {
+              return callback(err);
+            }
+            expect(ledgerNode).to.be.ok;
+            callback(null, ledgerNode);
+          })]
+      }, (err, results) => {
+        if(err) {
+          return done(err);
+        }
+        commonActor = results.actor;
+        genesisLedgerNode = results.ledgerNode;
+        consensusApi = results.consensusPlugin.api;
+        done();
+      });
+    });
+
+    // get genesis record (block + meta)
+    let genesisRecord;
+    before(done => {
+      genesisLedgerNode.blocks.getGenesis((err, result) => {
+        if(err) {
+          return done(err);
+        }
+        genesisRecord = result.genesisBlock;
+      });
+    });
+
+    // add N - 1 more private nodes
+    const peers = [genesisLedgerNode];
+    before(done => {
+      let count = 0;
+      async.whilst(() => count++ < (nodes - 1), loop => {
+        brLedger.add(
+          commonActor, {genesisBlock: genesisRecord.block}, (err, ledgerNode) => {
+            if(err) {
+              return loop(err);
+            }
+            peers.push(ledgerNode);
+            loop();
+          });
+      }, done);
+    });
+
+    it('should add an event and achieve consensus', done => {
+      const testEvent = bedrock.util.clone(mockData.events.alpha);
+      testEvent.input[0].id = 'https://example.com/events/' + uuid();
+      async.auto({
+        addEvent: callback => genesisLedgerNode.events.add(
+          testEvent, callback),
+        runWorkers: ['addEvent', (results, callback) => async.each(
+          peers,
+          (ledgerNode, callback) => consensusApi._worker._run(null, callback),
+          callback)],
+        wait: ['runWorkers', (results, callback) => {
+          setTimeout(callback, maxConsensusTime);
+        }],
+        getLatest: ['runWorkers', (results, callback) =>
+          async.each(peers, (ledgerNode, callback) =>
+            ledgerNode.storage.blocks.getLatest((err, result) => {
+              should.not.exist(err);
+              const eventBlock = result.eventBlock;
+              should.exist(eventBlock.block);
+              eventBlock.block.event.should.be.an('array');
+              eventBlock.block.event.should.have.length(1);
+              const event = eventBlock.block.event[0];
+              event.input.should.be.an('array');
+              event.input.should.have.length(1);
+              event.should.deep.equal(testEvent);
+              should.exist(eventBlock.meta);
+              callback();
+          }, callback))]
+      }, done);
+    });
+  });
+});
