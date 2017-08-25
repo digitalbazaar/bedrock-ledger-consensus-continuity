523e6dcc4046cfb27b9d60162ddba6c587f2b8d9
Matthew Collier <collier.matthew@gmail.com>
2017-08-25 18:28:43 -0400

    Revert "Add voting round interval."
    This reverts commit efbc3b28ef63939cab89507e35be496a8f19bb74.

--- test/test.config.js
+++ test/test.config.js
@@ -12,6 +12,3 @@ config.jsonld.strictSSL = false;
 config.mongodb.name = 'bedrock_ledger_continuity_test';
 config.mongodb.dropCollections.onInit = true;
 config.mongodb.dropCollections.collections = [];
-
-// reduce processing interval for testing
-config['ledger-consensus-continuity'].worker.election.votingRoundInterval = 0;
