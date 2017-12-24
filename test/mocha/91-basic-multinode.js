a864ce0262e0bc4fef2456d328d426be6dd4377a
Dave Longley <dlongley@digitalbazaar.com>
2017-12-23 22:58:02 -0500

    Do not include regular events in `treeChildren`.

--- test/mocha/91-basic-multinode.js
+++ test/mocha/91-basic-multinode.js
@@ -133,8 +133,10 @@ describe.only('Multinode Basics', () => {
           betaAddEvent1: callback => nodes.beta.events.add(
             helpers.createEventBasic({eventTemplate}), callback),
           // beta will merge its new regular event
-          betaWorker1: ['betaAddEvent1', (results, callback) =>
-            consensusApi._worker._run(nodes.beta, callback)],
+          betaWorker1: ['betaAddEvent1', (results, callback) => {
+            console.log('running beta worker 1 ------------');
+            consensusApi._worker._run(nodes.beta, callback);
+          }],
           test1: ['betaWorker1', (results, callback) => async.auto({
             alpha: callback => nodes.alpha.storage.events.collection.find({})
               .toArray((err, result) => {
@@ -150,11 +152,13 @@ describe.only('Multinode Basics', () => {
               }),
           }, callback)],
           // beta will push the regular and merge events to alpha
-          betaWorker2: ['test1', (results, callback) =>
+          betaWorker2: ['test1', (results, callback) => {
+            console.log('running beta worker 2 ------------');
             consensusApi._worker._run(nodes.beta, err => {
               assertNoError(err);
               callback(err);
-            })],
+            });
+          }],
           test2: ['betaWorker2', (results, callback) => async.auto({
             alpha: callback => nodes.alpha.storage.events.collection.find({})
               .toArray((err, result) => {
@@ -170,14 +174,17 @@ describe.only('Multinode Basics', () => {
               }),
           }, callback)],
           // alpha will merge the event received from beta and create a block
-          alphaWorker1: ['test2', (results, callback) =>
+          alphaWorker1: ['test2', (results, callback) => {
+            console.log('running alpha worker 1 ------------');
             consensusApi._worker._run(nodes.alpha, err => {
               assertNoError(err);
               callback(err);
-            })],
+            });
+          }],
           test3: ['alphaWorker1', (results, callback) =>
             nodes.alpha.storage.blocks.getLatest((err, result) => {
               assertNoError(err);
+              console.log('testing for first block ---------------');
               // first block has no proof because alpha is only elector
               result.eventBlock.block.consensusProof.should.have.length(0);
               result.eventBlock.block.blockHeight.should.equal(1);
@@ -385,12 +392,18 @@ describe.only('Multinode Basics', () => {
           alphaAddEvent1: callback => nodes.alpha.events.add(
             helpers.createEventBasic({eventTemplate}), callback),
           // beta will merge its new regular event
-          workCycle1: ['alphaAddEvent1', (results, callback) =>
-            _workerCycle({consensusApi, nodes}, callback)],
-          workCycle2: ['workCycle1', (results, callback) =>
-            _workerCycle({consensusApi, nodes}, callback)],
-          workCycle3: ['workCycle2', (results, callback) =>
-            _workerCycle({consensusApi, nodes}, callback)],
+          workCycle1: ['alphaAddEvent1', (results, callback) => {
+            console.log('WORKER CYCLE 1 ---------------------');
+            _workerCycle({consensusApi, nodes}, callback);
+          }],
+          workCycle2: ['workCycle1', (results, callback) => {
+            console.log('WORKER CYCLE 2 ---------------------');
+            _workerCycle({consensusApi, nodes}, callback);
+          }],
+          workCycle3: ['workCycle2', (results, callback) => {
+            console.log('WORKER CYCLE 3 ---------------------');
+            _workerCycle({consensusApi, nodes}, callback);
+          }],
           // workCycle4: ['workCycle3', (results, callback) =>
           //   _workerCycle({consensusApi, nodes}, callback)],
 
@@ -406,26 +419,20 @@ describe.only('Multinode Basics', () => {
           betaWorker3: ['alphaWorker2', (results, callback) =>
             consensusApi._worker._run(nodes.beta, callback)],
           // new merge events are continually generated on alpha, must end
-          // with a beta worker to equalizet the events
-
-          // NOTE: this test shows that two nodes with the same events are
-          // not reaching consensus at the same time.  Unclear if beta is
-          // reaching consenus too soon or alpha not soon enough.  Alpha does
-          // not move from blockHeight 2 as can be shows from the tests
-          // `makes many more blocks` below
+          // with a beta worker to equalize the events
 
           test10: ['betaWorker3', (results, callback) => async.auto({
             alpha: callback => nodes.alpha.storage.events.collection.find({})
               .toArray((err, result) => {
                 assertNoError(err);
-                result.should.have.length(24);
+                result.should.have.length(23);
                 callback(err, result.map(e => e.eventHash));
               }),
             beta: ['alpha', (results, callback) =>
               nodes.beta.storage.events.collection.find({})
                 .toArray((err, result) => {
                   assertNoError(err);
-                  result.should.have.length(24);
+                  result.should.have.length(23);
                   result.map(e => e.eventHash)
                     .should.have.same.members(results.alpha);
                   callback();
@@ -434,20 +441,14 @@ describe.only('Multinode Basics', () => {
               nodes.alpha.storage.blocks.getLatest(
                 (err, result) => {
                   assertNoError(err);
-
-                  // FIXME: should this be 3??
-                  result.eventBlock.block.blockHeight.should.equal(2);
-
+                  result.eventBlock.block.blockHeight.should.equal(4);
                   callback();
                 })],
             betaBlock: ['alphaBlock', (results, callback) =>
               nodes.beta.storage.blocks.getLatest(
                 (err, result) => {
                   assertNoError(err);
-
-                  // FIXME: should this be 2, 3?
                   result.eventBlock.block.blockHeight.should.equal(4);
-
                   callback();
                 })],
           }, callback)],
