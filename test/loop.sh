17fa459f3aabe6573395ba5b0f7636fdee4dfc2b
Matthew Collier <collier.matthew@gmail.com>
2018-01-16 16:17:07 -0500

    Log errors to separate file.

--- test/loop.sh
+++ test/loop.sh
@@ -5,6 +5,6 @@ do
 ((++counter))
 echo 'Pass ==================' $counter
 curl -k -d '{"pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://34.237.218.127:18443/test-hub
-npm test >test-pass.log 2>&1
+npm test >test-pass.log 2>test-error.log
 done
 curl -k -d '{"pass":"'${counter}'", "status":"failed"}' -H "Content-Type: application/json" -X POST https://34.237.218.127:18443/test-hub
