408cc8aae41e2ddc784b2fc7daf4c68238f243d4
Matthew Collier <collier.matthew@gmail.com>
2018-01-17 23:17:03 -0500

    Update address to test hub.

--- test/loop.sh
+++ test/loop.sh
@@ -4,7 +4,7 @@ while [ $? -eq 0 ]
 do
 ((++counter))
 echo 'Pass ==================' $counter
-curl -k -d '{"pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://34.237.218.127:18443/test-hub
+curl -k -d '{"pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
 npm test >/dev/shm/test-pass.log 2>/dev/shm/test-error.log
 done
-curl -k -d '{"pass":"'${counter}'", "status":"failed"}' -H "Content-Type: application/json" -X POST https://34.237.218.127:18443/test-hub
+curl -k -d '{"pass":"'${counter}'", "status":"failed"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
