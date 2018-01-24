67a20cfcbd434656809d1f43554f5fe20b1f7ef7
Matthew Collier <collier.matthew@gmail.com>
2018-01-24 10:43:24 -0500

    Add commit to loop transmission.

--- test/loop.sh
+++ test/loop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+commit="$(git rev-parse --short HEAD)"
 counter=0
 while [ $? -eq 0 ]
 do
 ((++counter))
 echo 'Pass ==================' $counter
-curl -k -d '{"pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
+curl -k -d '{"commit":"'${commit}'", "pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
 npm test >/dev/shm/test-pass.log 2>/dev/shm/test-error.log
 done
 curl -k -d '{"pass":"'${counter}'", "status":"failed"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
