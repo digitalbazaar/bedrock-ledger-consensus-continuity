5aa122c60545dac57afdeda0727ff8cf6d55f1a0
Matthew Collier <collier.matthew@gmail.com>
2018-10-02 15:21:38 -0400

    Add timestamp to loop.

--- test/loop.sh
+++ test/loop.sh
@@ -4,7 +4,7 @@ counter=0
 while [ $? -eq 0 ]
 do
 ((++counter))
-echo 'Pass ==================' $counter
+echo $(date) 'Pass ==================' $counter
 # curl -k -d '{"commit":"'${commit}'", "pass":"'${counter}'", "status":"running"}' -H "Content-Type: application/json" -X POST https://ec2-34-201-11-46.compute-1.amazonaws.com:18443/test-hub
 npm test >/dev/shm/test-pass.log 2>/dev/shm/test-error.log
 done
