081c4988ae4c272abaf2ff560abab48a3af86712
Matthew Collier <collier.matthew@gmail.com>
2018-10-08 17:12:03 -0400

    Add tests.

--- test/mocha/101-recovery-mode.js
+++ test/mocha/101-recovery-mode.js
@@ -36,17 +36,27 @@ describe.only('Recovery mode simulation', () => {
     const electorSelectionApi = brLedgerNode.use(
       'MostRecentParticipantsWithRecovery');
 
-    electorSelectionApi.api._computeElectors = async () => {
+    let savedElectors;
+    electorSelectionApi.api._computeElectors = async ({blockHeight}) => {
+      // electors only need to be computed once
+      if(savedElectors) {
+        return savedElectors;
+      }
       const electors = [];
       for(const p of Object.keys(peers)) {
         electors.push({id: peers[p]});
       }
-      console.log(`${Date.now()} _COMPUTEELECTORS`, electors.length);
+      savedElectors = electors;
       return electors;
     };
 
+    // since the recoveryElectors in this test are based on `nodes` which
+    // changes throughout the tests as nodes are removed and added, the
+    // value computed at each blockHeight is stored for the benefit of nodes
+    // that must catch up as they are re-enabled.
+    const recoveryElectorsByBlockHeight = new Map();
     electorSelectionApi.api._computeRecoveryElectors =
-      ({electors, f}) => {
+      ({blockHeight, electors, f}) => {
         // const activePeers = new Set();
         let recoveryElectors = [];
         for(const n of Object.keys(nodes)) {
@@ -54,7 +64,13 @@ describe.only('Recovery mode simulation', () => {
           recoveryElectors.push({id: peers[n]});
         }
 
+        let r = recoveryElectorsByBlockHeight.get(blockHeight);
+        if(r) {
+          return r;
+        }
+
         if(electors.length === 1) {
+          recoveryElectorsByBlockHeight.set(blockHeight, []);
           return [];
         }
 
@@ -71,7 +87,9 @@ describe.only('Recovery mode simulation', () => {
           .sort((a, b) => a.id.localeCompare(b.id)).slice(0, f + 1);
         console.log(`${Date.now()} _COMPUTERECOVERYELECTORS, f`,
           Object.keys(nodes), recoveryElectors, f);
-        return recoveryElectors.length < f + 1 ? [] : recoveryElectors;
+        r = recoveryElectors.length < f + 1 ? [] : recoveryElectors;
+        recoveryElectorsByBlockHeight.set(blockHeight, r);
+        return r;
       };
     // the return value here gets multiplied by 10
     electorSelectionApi.api._computeRecoveryMinimumMergeEvents = () => 2;
@@ -198,7 +216,7 @@ describe.only('Recovery mode simulation', () => {
     });
 
     /*
-     * 1. add new unique operations/records on nodes alpha, beta, gamma, delta
+     * 1. add new unique operations/records on *all* nodes
      * 2. run worker on *all* nodes
      * 3. repeat 1 and 2 until target block height is reached on all nodes
      * 4. ensure that blockHash for the target block height is identical on all
@@ -206,20 +224,41 @@ describe.only('Recovery mode simulation', () => {
      * 6. ensure that the final blockHeight and blockHash is identical on all
      * 7. attempt to retrieve all records added in 1 from the `records` API
      *
-     * Test recovery mode:
+     * Test recovery mode with four active nodes:
      * 8. Reduce `nodes` from seven to four nodes: alpha, beta, gamma, delta
      * 9. add new unique operations/records on nodes alpha, beta, gamma, delta
      * 10. run worker on all remaining nodes
      * 11. repeat 9 and 10 until target block height is reached on all nodes
      *     it is anticipated that the network should go into recovery mode
      *     under these conditions.
+     * 12. attempt to retrieve all records added in 9 from the `records` API
+     *
+     * Test recovery mode with three active nodes:
+     * 13. Reduce `nodes` from four to three nodes: alpha, beta, gamma
+     * 14. add new unique operations/records on nodes alpha, beta, gamma
+     * 15. run worker on all remaining nodes
+     * 16. repeat 9 and 10 until target block height is reached on all nodes
+     *     it is anticipated that the network should go into recovery mode
+     *     under these conditions.
+     * 17. attempt to retrieve all records added in 14 from the `records` API
+     *
+     * Restore network to normal operation with all 7 nodes
+     * 18. Add all previously disabled nodes back into `nodes`
+     * 19. Settle the network to allow the previously disabled nodes to catch up
+     * 20. add new unique operations/records on *all* nodes
+     * 21. run worker on *all* nodes
+     * 22. repeat 20 and 21 until target block height is reached on all nodes
+     * 23. ensure that blockHash for the target block height is identical on all
+     * 24. settle the network, see notes on _settleNetwork
+     * 25. ensure that the final blockHeight and blockHash is identical on all
+     * 26. attempt to retrieve all records added in 1 from the `records` API
      */
 
     const targetBlockHeight = 5;
     let stageOneBlockHeight;
     describe(`${targetBlockHeight} Blocks`, () => {
       it(`makes ${targetBlockHeight} blocks with all nodes`, function(done) {
-        this.timeout(0);
+        this.timeout(180000);
         async.auto({
           nBlocks: callback => helpers.nBlocks({
             consensusApi, nodes, opTemplate, operationOnWorkCycle: 'all',
@@ -282,11 +321,11 @@ describe.only('Recovery mode simulation', () => {
       });
     }); // end one block
 
-    const recoveryBlocksFourNodes = 10;
+    const recoveryBlocksFourNodes = 5;
     let stageTwoBlockHeight;
     describe(`${recoveryBlocksFourNodes} Recovery Blocks`, () => {
       it(`makes ${recoveryBlocksFourNodes} blocks w/4 nodes`, function(done) {
-        this.timeout(0);
+        this.timeout(180000);
 
         const newTargetBlockHeight = stageOneBlockHeight +
           recoveryBlocksFourNodes;
@@ -357,10 +396,11 @@ describe.only('Recovery mode simulation', () => {
       });
     }); // end recovery blocks four nodes
 
-    const recoveryBlocksThreeNodes = 10;
+    const recoveryBlocksThreeNodes = 5;
+    let stageThreeBlockHeight;
     describe(`${recoveryBlocksThreeNodes} Recovery Blocks`, () => {
       it(`makes ${recoveryBlocksThreeNodes} blocks w/3 nodes`, function(done) {
-        this.timeout(0);
+        this.timeout(180000);
 
         const newTargetBlockHeight = stageTwoBlockHeight +
           recoveryBlocksThreeNodes;
@@ -392,6 +432,8 @@ describe.only('Recovery mode simulation', () => {
               if(err) {
                 return callback(err);
               }
+              stageThreeBlockHeight = result[Object.keys(result)[0]].eventBlock
+                .block.blockHeight;
               const summaries = {};
               Object.keys(result).forEach(k => {
                 summaries[k] = {
@@ -427,7 +469,122 @@ describe.only('Recovery mode simulation', () => {
           done();
         });
       });
-    }); // end recovery blocks four nodes
+    }); // end recovery blocks three nodes
+
+    const regularBlocksSevenNodes = 10;
+    let stageFourBlockHeight;
+    describe(`${regularBlocksSevenNodes} Regular Blocks`, () => {
+      it(`makes ${regularBlocksSevenNodes} blocks w/7 nodes`, function(done) {
+        this.timeout(180000);
+
+        const newTargetBlockHeight = stageThreeBlockHeight +
+          regularBlocksSevenNodes;
+
+        // enable all previously disabled nodes, back to seven nodes
+        _enableNodes(['delta', 'epsilon', 'zeta', 'eta']);
+
+        async.auto({
+          // let the previously disabled nodes catch up
+          settle: callback => helpers.settleNetwork(
+            {consensusApi, nodes: _.values(nodes)}, callback),
+          blockSummary: ['settle', (results, callback) =>
+            _latestBlockSummary((err, result) => {
+              if(err) {
+                return callback(err);
+              }
+              const summaries = {};
+              Object.keys(result).forEach(k => {
+                summaries[k] = {
+                  blockCollection: nodes[k].storage.blocks.collection.s.name,
+                  blockHeight: result[k].eventBlock.block.blockHeight,
+                  blockHash: result[k].eventBlock.meta.blockHash,
+                  previousBlockHash: result[k].eventBlock.block
+                    .previousBlockHash,
+                };
+              });
+              console.log('Finishing block summaries:', JSON.stringify(
+                summaries, null, 2));
+              _.values(summaries).forEach(b => {
+                b.blockHeight.should.equal(summaries.alpha.blockHeight);
+                b.blockHash.should.equal(summaries.alpha.blockHash);
+              });
+              callback();
+            })],
+          nBlocks: ['blockSummary', (results, callback) => helpers.nBlocks({
+            consensusApi, nodes, opTemplate, operationOnWorkCycle: 'all',
+            targetBlockHeight: newTargetBlockHeight
+          }, (err, result) => {
+            if(err) {
+              return callback(err);
+            }
+            console.log(
+              'targetBlockHashMap', JSON.stringify(result, null, 2));
+            const firstNodeLabel = Object.keys(result.targetBlockHashMap)[0];
+            _.values(result.targetBlockHashMap)
+              .every(h => h === result.targetBlockHashMap[firstNodeLabel])
+              .should.be.true;
+
+            callback(null, result);
+          })],
+          settle2: ['nBlocks', (results, callback) => helpers.settleNetwork(
+            {consensusApi, nodes: _.values(nodes)}, callback)],
+          blockSummary2: ['settle2', (results, callback) =>
+            _latestBlockSummary((err, result) => {
+              if(err) {
+                return callback(err);
+              }
+              stageFourBlockHeight = result[Object.keys(result)[0]].eventBlock
+                .block.blockHeight;
+              const summaries = {};
+              Object.keys(result).forEach(k => {
+                summaries[k] = {
+                  blockCollection: nodes[k].storage.blocks.collection.s.name,
+                  blockHeight: result[k].eventBlock.block.blockHeight,
+                  blockHash: result[k].eventBlock.meta.blockHash,
+                  previousBlockHash: result[k].eventBlock.block
+                    .previousBlockHash,
+                };
+              });
+              console.log('Finishing block summaries:', JSON.stringify(
+                summaries, null, 2));
+              _.values(summaries).forEach(b => {
+                b.blockHeight.should.equal(summaries.alpha.blockHeight);
+                b.blockHash.should.equal(summaries.alpha.blockHash);
+              });
+              callback();
+            })],
+          state: ['blockSummary2', (results, callback) => {
+            const allRecordIds = [].concat(..._.values(
+              results.nBlocks.recordIds));
+            console.log(`Total operation count: ${allRecordIds.length}`);
+            async.eachSeries(allRecordIds, (recordId, callback) => {
+              nodes.alpha.records.get({recordId}, err => {
+                // just need to ensure that there is no NotFoundError
+                assertNoError(err);
+                callback();
+              });
+            }, callback);
+          }],
+          testParticipants: ['state', (results, callback) => {
+            const ledgerNode = nodes.alpha;
+            const {getParticipants} = ledgerNode.consensus._blocks;
+            getParticipants(
+              {blockHeight: stageFourBlockHeight, ledgerNode},
+              (err, result) => {
+                assertNoError(err);
+                // proof on the last block created should involve all 7 nodes
+                result.consensusProofPeers.should.have.length(7);
+                result.consensusProofPeers.should.have.same.members(
+                  _.values(peers));
+                callback();
+              });
+          }]
+        }, err => {
+          assertNoError(err);
+          done();
+        });
+      });
+    }); // end regular blocks seven nodes
   });
 });
 
@@ -435,6 +592,13 @@ function _disableNodes(nodeLabels) {
   for(const node of nodeLabels) {
     disabledNodes[node] = nodes[node];
     delete nodes[node];
+  }
+}
+
+function _enableNodes(nodeLabels) {
+  for(const node of nodeLabels) {
+    nodes[node] = disabledNodes[node];
+    delete disabledNodes[node];
   }
 }
 
