098d4b0eee33bac4286cb483eb53d0c63b67118a
Matthew Collier <collier.matthew@gmail.com>
2018-12-17 13:53:08 -0500

    Add validation to server endpoints.

--- schemas/continuity-server.js
+++ schemas/continuity-server.js
@@ -25,4 +25,45 @@ const getEvents = {
   }
 };
 
+// TODO: can this be more specific?
+const peerId = {
+  type: 'string',
+};
+
+const gossip = {
+  title: 'Continuity Gossip',
+  // ensure that `headsOnly` and `creatorHeads` are mutually exclusive
+  anyOf: [{
+    type: 'object',
+    required: ['headsOnly', 'peerId'],
+    additionalProperties: false,
+    properties: {
+      headsOnly: {
+        type: 'boolean',
+      },
+      peerId,
+    }
+  }, {
+    type: 'object',
+    required: ['creatorHeads', 'peerId'],
+    additionalProperties: false,
+    properties: {
+      creatorHeads: {
+        type: 'object',
+      },
+      peerId,
+    }
+  }]
+};
+
+const notification = {
+  title: 'Continuity Server Gossip Notification',
+  type: 'object',
+  additionalProperties: false,
+  require: ['peerId'],
+  properties: {peerId}
+};
+
 module.exports.getEvents = () => getEvents;
+module.exports.gossip = () => gossip;
+module.exports.notification = () => notification;
