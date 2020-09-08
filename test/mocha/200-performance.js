566cd27435090da8b26055267db08421431551d4
Andrew L Jones <ajones@digitalbazaar.com>
2020-09-08 16:23:40 -0400

    Update test 200 to async.

--- test/mocha/200-performance.js
+++ test/mocha/200-performance.js
@@ -22,37 +22,17 @@ describe.skip('Performance - Consensus Client - getBlockStatus API', () => {
   let ledgerNode;
   let voterId;
   let testEventId;
-  before(done => {
+  before(async function() {
     const ledgerConfiguration = mockData.ledgerConfiguration;
     const testEvent = bedrock.util.clone(mockData.events.alpha);
     testEventId = 'https://example.com/events/' + uuid();
     testEvent.operation[0].record.id = testEventId;
-    async.auto({
-      clean: callback =>
-        helpers.removeCollections(['ledger', 'ledgerNode'], callback),
-      consensusPlugin: callback =>
-        helpers.use('Continuity2017', (err, result) => {
-          if(err) {
-            return callback(err);
-          }
-          consensusApi = result.api;
-          callback();
-        }),
-      ledgerNode: ['clean', (results, callback) => brLedgerNode.add(
-        null, {ledgerConfiguration}, (err, result) => {
-          if(err) {
-            return callback(err);
-          }
-          ledgerNode = result;
-          callback();
-        })],
-      getVoter: ['consensusPlugin', 'ledgerNode', (results, callback) => {
-        consensusApi._voters.get(ledgerNode.id, (err, result) => {
-          voterId = result.id;
-          callback();
-        });
-      }]
-    }, done);
+    await helpers.removeCollections(['ledger', 'ledgerNode']);
+    const consensusPlugin = helpers.use('Continuity2017');
+    consensusApi = consensusPlugin.api;
+    ledgerNode = await brLedgerNode.add(null, {ledgerConfiguration});
+    const voter = await consensusApi._voters.get({ledgerNodeId: ledgerNode.id});
+    voterId = voter.id;
   });
   describe('Preparation', () => {
     it(`adds ${eventNum} events`, function(done) {
