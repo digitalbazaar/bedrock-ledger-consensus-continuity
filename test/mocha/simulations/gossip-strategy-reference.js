69e2dcbb7ab64713e2f1ee6939e53884f195a432
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-12 16:30:02 -0500

    Refactor pipeline files.

---
+++ test/mocha/simulations/gossip-strategy-reference.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const _ = require('lodash');
+
+module.exports.run = async function() {
+  // run peer selector algorithm
+  const peers = await this.getPeers();
+  const idx = Math.round(Math.random() * (peers.length - 1));
+  const peer = peers[idx]; // select random peer
+
+  // get different histories
+  const nodeHistory = await this.getHistory();
+  const peerHistory = await peer.getHistory();
+
+  // get different events
+  const nodeEvents = nodeHistory.events.map(({eventHash}) => eventHash);
+  const peerEvents = peerHistory.events.map(({eventHash}) => eventHash);
+
+  // diff histories
+  const diff = _.difference(peerEvents, nodeEvents);
+
+  // download events from peer
+  const downloadedEvents = await peer.getEvents({events: diff});
+
+  // add events to node
+  await this.addEvents({events: downloadedEvents});
+
+  return {peer: peer.nodeId, events: downloadedEvents.length};
+};
