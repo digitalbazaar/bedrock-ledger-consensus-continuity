cbaa94bcb74a1c71f636b4b411230b88b7c61004
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-13 18:40:22 -0500

    Update mongodb-client.

--- test/simulate-cli/mongo-client.js
+++ test/simulate-cli/mongo-client.js
@@ -12,31 +12,17 @@ if(!mongoUrl) {
 }
 
 module.exports.send = async function({payload}) {
-  const {collection, db} = await _initMongo2();
+  const {client, collection} = await _initMongo();
   await collection.insert(payload);
-  await _closeMongo({db});
+  await _closeMongo({client});
 };
 
-async function _closeMongo({db}) {
-  await db.close();
-}
-
-async function _initMongo2() {
-  const {db} = await new Promise((resolve, reject) => {
-    MongoClient.connect(mongoUrl, {}, (err, db) => {
-      if(err) {
-        return reject(err);
-      }
-      resolve({db});
-    });
-  });
-  const collection = db.collection('hackathon_reports');
-  // TODO: indexes?
-  return {collection, db};
+async function _closeMongo({client}) {
+  await client.close();
 }
 
 // eslint-disable-next-line no-unused-vars
-async function _initMongo3() {
+async function _initMongo() {
   const client = new MongoClient(mongoUrl, {useUnifiedTopology: true});
   await client.connect();
   const db = client.db();
