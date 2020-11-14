98731d0f60893a51fde52ad0a3121ff6bed8695c
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-14 18:46:27 -0500

    Use fast random id generation.

--- test/mocha/tools/helpers.js
+++ test/mocha/tools/helpers.js
@@ -2,9 +2,8 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
-const crypto = require('crypto');
-const util = require('util');
-const randomBytes = util.promisify(crypto.randomBytes);
+
+const {nanoid} = require('./nanoid-non-secure');
 
 const api = {};
 module.exports = api;
@@ -14,9 +13,8 @@ api.strfy = a => JSON.stringify(a, null, 2);
 api.generateId = generateId;
 api.getTimer = getTimer;
 
-async function generateId({encoding = 'base64', bytes = 8} = {}) {
-  const buffer = await randomBytes(bytes);
-  return buffer.toString(encoding);
+async function generateId({length = 12} = {}) {
+  return nanoid(length);
 }
 
 function getTimer() {
