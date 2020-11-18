bc765d81a2de3e9ae80506bf8ed9f4e09f94221b
Dave Longley <dlongley@digitalbazaar.com>
2020-11-18 13:49:17 -0500

    Add infrastructure to support non-witness nodes.

--- test/mocha/tools/Simulator.js
+++ test/mocha/tools/Simulator.js
@@ -8,7 +8,8 @@ const Node = require('./Node');
 const uuid = require('uuid-random');
 
 class Simulator {
-  constructor({id, name, creator, run, witnessCount, pipeline, init} = {}) {
+  constructor(
+    {id, name, creator, run, nodeCount, witnessCount, pipeline, init} = {}) {
     if((witnessCount - 1) % 3 !== 0) {
       throw new Error(`Witness Count must equal "3f + 1".`);
     }
@@ -19,22 +20,23 @@ class Simulator {
     this.timestamp = Date.now();
     this.creator = creator;
     this.graph = new Graph();
+    this.nodes = new Map();
     this.witnesses = new Map();
     this.tick = 0;
 
-    this._createWitnesses({count: witnessCount, pipeline});
+    this._createNodes({nodeCount, witnessCount, pipeline});
 
     if(init) {
       return init(this);
     }
 
-    this._defaultInitialization({count: witnessCount});
+    this._defaultInitialization({nodeCount, witnessCount});
   }
 
   continue() {
     let stop = true;
-    for(const witness of this.witnesses.values()) {
-      stop = stop && witness.hasConsensus;
+    for(const node of this.nodes.values()) {
+      stop = stop && node.hasConsensus;
     }
     return !stop;
   }
@@ -45,11 +47,11 @@ class Simulator {
     while(this.continue() && this.tick <= MAX_TICKS) {
       this.tick++;
       console.log(`\n========== TICK ${this.tick} ==========`);
-      for(let i = 0; i < this.witnesses.size; i++) {
+      for(let i = 0; i < this.nodes.size; i++) {
         const nodeId = this._nodeId(i);
         console.log(`\n++++++++++ Node ${nodeId} ++++++++++`);
-        const witness = this.witnesses.get(nodeId);
-        await witness.tick({id: this.tick});
+        const node = this.nodes.get(nodeId);
+        await node.tick({id: this.tick});
       }
     }
 
@@ -64,12 +66,12 @@ class Simulator {
     const gossipSessions = {};
 
     // Activity Log
-    for(let i = 0; i < this.witnesses.size; i++) {
+    for(let i = 0; i < this.nodes.size; i++) {
       const nodeId = this._nodeId(i);
-      const witness = this.witnesses.get(nodeId);
+      const node = this.nodes.get(nodeId);
 
       // find earliest consensus
-      const earliestConsensus = witness.activityLog.find(({type, details}) => {
+      const earliestConsensus = node.activityLog.find(({type, details}) => {
         if(type === 'consensus' && details.consensus === true) {
           return true;
         }
@@ -78,7 +80,7 @@ class Simulator {
       consensusReports.push(earliestConsensus);
 
       // calculate total gossip sessions
-      const gossips = witness.activityLog.filter(({type}) => type === 'gossip');
+      const gossips = node.activityLog.filter(({type}) => type === 'gossip');
       const downloadedEventsTotal = gossips.reduce((acc, curr) => {
         acc += curr.details.events;
         return acc;
@@ -112,6 +114,7 @@ class Simulator {
       return acc;
     }, baseResult);
 
+    const nodeCount = this.nodes.size;
     const witnessCount = this.witnesses.size;
 
     const f = (witnessCount - 1) / 3;
@@ -127,33 +130,45 @@ class Simulator {
       targetMergeEvents,
       average: results,
       gossipSessions,
+      nodeCount,
       witnessCount,
     };
   }
 
-  _defaultInitialization({count}) {
-    for(let i = 0; i < count; i++) {
+  _defaultInitialization({witnessCount, nodeCount = witnessCount}) {
+    for(let i = 0; i < nodeCount; i++) {
       const nodeId = this._nodeId(i);
 
       // create nodes in graph
       this.graph.addNode(nodeId);
-      this.graph.mergeEvent({eventHash: `y${nodeId}`, to: nodeId, from: []});
+      const node = this.nodes.get(nodeId);
+      // `y` means little `y` from the consensus algorithm, only witnesses
+      // make these; `i` means initial event, for non-witnesses always make
+      // one of these to get started as well
+      const eventHash = node.isWitness ? `y${nodeId}` : `i${nodeId}`;
+      this.graph.mergeEvent({eventHash, to: nodeId, from: []});
 
-      const witness = this.witnesses.get(nodeId);
-      witness.totalMergeEventsCreated++;
+      node.totalMergeEventsCreated++;
     }
   }
 
-  _createWitnesses({count, pipeline}) {
-    for(let i = 0; i < count; i++) {
+  _createNodes({witnessCount, nodeCount = witnessCount, pipeline}) {
+    // create nodes
+    for(let i = 0; i < nodeCount; i++) {
       const nodeId = this._nodeId(i);
+      const isWitness = i < witnessCount;
+
+      // add node to map
+      const node = new Node({
+        nodeId, pipeline, graph: this.graph, witnesses: this.witnesses,
+        isWitness
+      });
+      this.nodes.set(nodeId, node);
 
       // add witness to map
-      const witness = new Node({
-        nodeId, pipeline, graph: this.graph, witnesses: this.witnesses,
-        isWitness: true
-      });
-      this.witnesses.set(nodeId, witness);
+      if(isWitness) {
+        this.witnesses.set(nodeId, node);
+      }
     }
   }
 
