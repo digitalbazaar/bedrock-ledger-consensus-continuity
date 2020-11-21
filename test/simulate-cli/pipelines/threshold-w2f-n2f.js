ef7fc90215bb4cd0bc41a7e8c5b6ce26ac88fc7d
Manu Sporny <msporny@digitalbazaar.com>
2020-11-20 21:24:00 -0500

    Use random gossip with fanout of 2 for threshold pipelines.

--- test/simulate-cli/pipelines/threshold-w2f-n2f.js
+++ test/simulate-cli/pipelines/threshold-w2f-n2f.js
@@ -3,14 +3,14 @@
  */
 'use strict';
 
-const gossipStrategy = require('../gossip-strategies/previous-peer');
+const gossipStrategy = require('../gossip-strategies/reference');
 const mergeStrategy =
   require('../merge-strategies/threshold-merge');
 
 // NOTE: no spaces allowed, must be safe for prometheus metrics
 module.exports.name = 'threshold-w2f-n2f';
 module.exports.pipeline = async function() {
-  const count = 1;
+  const count = 2;
 
   for(let i = 0; i < count; i++) {
     await this.run({type: 'gossip', fn: gossipStrategy.run});
