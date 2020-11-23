066b7345e249453c2b43278e22b304f901ab6362
Manu Sporny <msporny@digitalbazaar.com>
2020-11-23 11:23:25 -0500

    Update threshold pipelines to vary operation ready chances.

--- test/simulate-cli/pipelines/threshold-wt2f-wm1-pm0-or020.js
+++ test/simulate-cli/pipelines/threshold-wt2f-wm1-pm0-or020.js
@@ -8,7 +8,7 @@ const mergeStrategy =
   require('../merge-strategies/threshold-merge');
 
 // NOTE: no spaces allowed, must be safe for prometheus metrics
-module.exports.name = 'threshold-wt2f-wmf-pm1';
+module.exports.name = 'threshold-wt2f-wm1-pm0-or020';
 module.exports.pipeline = async function() {
   const count = 2;
 
@@ -17,8 +17,8 @@ module.exports.pipeline = async function() {
   }
   const mergeArgs = {
     witnessTargetThreshold: '2f',
-    witnessMinimumThreshold: 'f',
-    peerMinimumThreshold: '1',
+    witnessMinimumThreshold: '1',
+    peerMinimumThreshold: '0',
     operationReadyChance: 0.2
   };
   await this.run({type: 'merge', fn: mergeStrategy.run, args: mergeArgs});
---
+++ test/simulate-cli/pipelines/threshold-wt2f-wmf-pm0-or020.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const gossipStrategy = require('../gossip-strategies/reference');
+const mergeStrategy =
+  require('../merge-strategies/threshold-merge');
+
+// NOTE: no spaces allowed, must be safe for prometheus metrics
+module.exports.name = 'threshold-wt2f-wmf-pm0-or020';
+module.exports.pipeline = async function() {
+  const count = 2;
+
+  for(let i = 0; i < count; i++) {
+    await this.run({type: 'gossip', fn: gossipStrategy.run});
+  }
+  const mergeArgs = {
+    witnessTargetThreshold: '2f',
+    witnessMinimumThreshold: 'f',
+    peerMinimumThreshold: '0',
+    operationReadyChance: 0.2
+  };
+  await this.run({type: 'merge', fn: mergeStrategy.run, args: mergeArgs});
+  const consensusResults = await this.run({type: 'consensus'});
+  if(consensusResults.consensus) {
+    console.log(`Found Consensus - Node ${this.nodeId}`);
+  }
+};
