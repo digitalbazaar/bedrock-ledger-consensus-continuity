47608ed0a3273a25915a9bfbcae6440145738bf0
Dave Longley <dlongley@digitalbazaar.com>
2021-03-02 17:03:36 -0500

    Make peer sampling cache configurable and include sample peers.

--- test/test.config.js
+++ test/test.config.js
@@ -14,7 +14,11 @@ config.mongodb.dropCollections.collections = [];
 
 // decrease delay for gossiping with the same peer
 config['ledger-consensus-continuity'].gossip.coolDownPeriod = 250;
+// limit event validation worker processes to 1
 config['ledger-consensus-continuity'].gossip.eventsValidation.workers = 1;
+// update peers very frequently for tests so that new peers are onboarded
+// and spread quickly for tests that typically only run for 30-60 seconds
+config['ledger-consensus-continuity'].gossip.peerSamplingCache.maxAge = 1000;
 
 config.mocha.options.bail = true;
 
