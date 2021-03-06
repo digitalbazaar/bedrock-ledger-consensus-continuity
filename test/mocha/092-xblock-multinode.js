7afeecc70bee675cbbfd9ba7b9bcda88256d4f19
Dave Longley <dlongley@digitalbazaar.com>
2021-03-06 17:40:27 -0500

    Remove FIXME.
    - We no longer clear backoff for notifications. This was a
      problem with the old scheme because backoff could become
      potentially days long. That is no longer an issue, maxing out
      at 5 minutes.

--- test/mocha/092-xblock-multinode.js
+++ test/mocha/092-xblock-multinode.js
@@ -22,7 +22,7 @@ const nodeLabels = [
 const nodes = {};
 const peers = {};
 
-describe('X Block Test', () => {
+describe.only('X Block Test', () => {
   before(async function() {
     this.timeout(TEST_TIMEOUT);
     await helpers.prepareDatabase();
