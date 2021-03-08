9a6956e774847be7b2d56b8bd57a1d464e16b19b
Dave Longley <dlongley@digitalbazaar.com>
2021-03-08 15:29:10 -0500

    Refresh peer cache quickly and further restrict notify peers selection.

--- test/test.config.js
+++ test/test.config.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2017-2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2017-2021 Digital Bazaar, Inc. All rights reserved.
  */
 const {config} = require('bedrock');
 const path = require('path');
@@ -16,9 +16,6 @@ config.mongodb.dropCollections.collections = [];
 config['ledger-consensus-continuity'].gossip.coolDownPeriod = 250;
 // limit event validation worker processes to 1
 config['ledger-consensus-continuity'].gossip.eventsValidation.workers = 1;
-// update peers very frequently for tests so that new peers are onboarded
-// and spread quickly for tests that typically only run for 30-60 seconds
-config['ledger-consensus-continuity'].gossip.peerCache.maxAge = 1000;
 
 config.mocha.options.bail = true;
 
