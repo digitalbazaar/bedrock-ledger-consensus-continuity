e85a7c27443adda77f66bf62b6be3b4e7bbf32cc
Ganesh Annan <gannan@digitalbazaar.com>
2021-06-23 23:01:36 -0400

    Fix typo.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -430,7 +430,7 @@ api.settleNetwork = async ({
       continue;
     }
 
-    // // all nodes should have the same operations
+    // all nodes should have the same operations
     promises = [];
     let recordLookupError = false;
     for(const ledgerNode of nodes) {
